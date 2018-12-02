dedb176a5cd502f6ef3b0fe8f789b4734bf96fe2
nicklasmoeller <hello@nicklasmoeller.com>
2018-12-02 18:23:40 +0100

    Add solution for day 02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -12,12 +12,68 @@ fn main() {
     println!("{}", part2(&input));
 }
 
-pub fn part1(_input: &str) -> isize {
-    0
+pub fn part1(input: &str) -> usize {
+    let mut twos: usize = 0;
+    let mut threes: usize = 0;
+
+    for line in input.lines() {
+        let mut has_two: bool = false;
+        let mut has_three: bool = false;
+
+        for c in line.chars() {
+            let count = line.matches(c).count();
+            if count == 2 {
+                has_two = true;
+            } else if count == 3 {
+                has_three = true;
+            }
+        }
+
+        if has_two {
+            twos += 1;
+        }
+        if has_three {
+            threes += 1;
+        }
+    }
+
+    twos * threes
 }
 
-pub fn part2(_input: &str) -> isize {
-    0
+pub fn part2(input: &str) -> String {
+    // NOTE: what's more readable? hashmap or nested for loop?
+    // going with nested for loop, since hashmap seems too easy
+    let lines: Vec<_> = input.lines().collect();
+
+    for (index, first_line) in lines.iter().enumerate() {
+        for next_line in lines.iter().skip(index + 1) {
+            if is_close(first_line, next_line) {
+                return get_common_chars(first_line, next_line);
+            }
+        }
+    }
+
+    fn is_close(first: &str, second: &str) -> bool {
+        if first.is_empty() {
+            false
+        } else if first.chars().nth(0) == second.chars().nth(0) {
+            is_close(&first[1..], &second[1..])
+        } else {
+            first[1..] == second[1..]
+        }
+    }
+
+    fn get_common_chars(first: &str, second: &str) -> String {
+        if first.chars().nth(0) == second.chars().nth(0) {
+            let mut result = String::from(&first[0..1]);
+            result.push_str(&get_common_chars(&first[1..], &second[1..]));
+            result
+        } else {
+            first[1..].to_string()
+        }
+    }
+
+    String::new()
 }
 
 #[cfg(test)]
@@ -26,11 +82,12 @@ mod tests {
 
     #[test]
     fn part1_examples() {
-        assert_eq!(true, true);
+        let input = "abcdef\nbababc\nabbcde\naabcdd\nabcccd\nabcdee\nababab";
+        assert_eq!(part1(input), 12);
     }
 
     #[test]
     fn part2_examples() {
-        assert_eq!(true, true);
+        assert_eq!(part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz"), "fgij");
     }
 }
