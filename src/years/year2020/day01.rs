4a9925d18318287d16df5c1a55ebeee6abe71e22
nicklasmoeller <hello@nicklasmoeller.com>
2020-12-01 23:41:43 +0100

    Day 1: Don't traverse already existed items

--- src/years/year2020/day01.rs
+++ src/years/year2020/day01.rs
@@ -6,35 +6,57 @@ impl AdventOfCode for Day01 {
     fn part_one(&self, input: &String) -> String {
         let lines = input
             .lines()
-            .map(|line| { line.parse::<usize>().expect("Not a number") })
+            .map(|line| line.parse::<usize>().expect("Not a number"))
             .collect::<Vec<usize>>();
 
-        lines.iter().find_map(|first| {
-            match lines.iter().find(|second| {
-                first + *second == 2020
-            }) {
-                Some(matching) => Some(first * matching),
-                None => None
-            }
-        }).expect("No matches").to_string()
+        lines
+            .iter()
+            .enumerate()
+            .find_map(|(first_index, first)| {
+                let next_index_from_first_traversed = first_index + 1;
+
+                match lines[next_index_from_first_traversed..]
+                    .iter()
+                    .find(|second| first + *second == 2020)
+                {
+                    Some(matching) => Some(first * matching),
+                    None => None,
+                }
+            })
+            .expect("No matches")
+            .to_string()
     }
 
     fn part_two(&self, input: &String) -> String {
         let lines = input
             .lines()
-            .map(|line| { line.parse::<usize>().expect("Not a number") })
+            .map(|line| line.parse::<usize>().expect("Not a number"))
             .collect::<Vec<usize>>();
 
-        lines.iter().find_map(|first| {
-            lines.iter().find_map(|second| {
-                match lines.iter().find(|third| {
-                    first + *second + *third == 2020
-                }) {
-                    Some(matching) => Some(first * second * matching),
-                    None => None
-                }
+        lines
+            .iter()
+            .enumerate()
+            .find_map(|(first_index, first)| {
+                let next_index_from_first_traversed = first_index + 1;
+
+                lines[next_index_from_first_traversed..]
+                    .iter()
+                    .enumerate()
+                    .find_map(|(second_index, second)| {
+                        let next_index_from_second_traversed =
+                            next_index_from_first_traversed + second_index + 1;
+
+                        match lines[next_index_from_second_traversed..]
+                            .iter()
+                            .find(|third| first + *second + *third == 2020)
+                        {
+                            Some(matching) => Some(first * second * matching),
+                            None => None,
+                        }
+                    })
             })
-        }).expect("No matches").to_string()
+            .expect("No matches")
+            .to_string()
     }
 }
 
@@ -57,6 +79,13 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "No matches")]
+    fn test_should_not_match_itself_part_one() {
+        let input = String::from("220\n1010");
+        (Day01 {}).part_one(&input);
+    }
+
+    #[test]
     fn test_examples_part_two() {
         let input = String::from("1721\n979\n366\n299\n675\n1456");
         assert_eq!((Day01 {}).part_two(&input), "241861950");
@@ -66,5 +95,12 @@ mod tests {
     fn test_input_part_two() {
         let input = read_to_string("data/2020/01.txt").expect("Could not read input file");
         assert_eq!((Day01 {}).part_two(&input), "116168640");
+    }
+
+    #[test]
+    #[should_panic(expected = "No matches")]
+    fn test_should_not_match_itself_part_two() {
+        let input = String::from("100\n20\n1000");
+        println!("{}", (Day01 {}).part_two(&input));
     }
 }
